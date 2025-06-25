f93b45765c370c2a92e6edba2f8a5467962465f9
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-06-25 15:45:23 +0530

    feat: create-mcmd-app

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,25 @@
 import { z } from "zod";
 import { defineCommand } from "mcmd";
 
+import util from "node:util";
+import child_process from "node:child_process";
+import fs from "node:fs";
+
+const exec = util.promisify(child_process.exec);
+const rm = util.promisify(fs.rm);
+
 export const options = z.object({
-    name: z.string()
+    name: z.string(),
 });
 
-export default defineCommand<typeof options>(({ name }) => {
-    console.log("Hi", name)
+export default defineCommand<typeof options>(async ({ name }) => {
+    const { stdout, stderr } = await exec(
+        `git clone https://github.com/rajatsandeepsen/create-mcmd-app.git ./${name}`,
+    );
+    stdout?.length && console.log(stdout);
+    stderr?.length && console.error(stderr);
+
+    await rm(`./${name}/.git`, { recursive: true, force: true });
+
+    console.log("MCMD Template cloned");
 });
